f0d7717f3347a8de0e2b35f4cfdea27ada19a7f8
Ryan W <w.ryan@stelch.com>
2019-07-09 01:01:36 +1000

    09/07/19 FRIENDS: - Starting porting over to redis env
    Removed ServerKick from BungeeCore (Now handled by main core)

--- src/gg/stelch/core/Commands/FriendsCommands/list.java
+++ src/gg/stelch/core/Commands/FriendsCommands/list.java
@@ -1,5 +1,6 @@
 package gg.stelch.core.Commands.FriendsCommands;
 
+import com.stelch.games2.core.BungeeCore;
 import com.stelch.games2.core.PlayerUtils.ProxyGamePlayer;
 import com.stelch.games2.core.PlayerUtils.ranks;
 import com.stelch.games2.core.Utils.SQL;
@@ -9,41 +10,43 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import redis.clients.jedis.Jedis;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Set;
 import java.util.UUID;
 
 public class list {
 
     public static void get(ProxiedPlayer p, String[] args) throws SQLException {
-        SQL sql = new SQL("35.192.213.70",3306,"root","Garcia#02","games");
 
-        ResultSet results = sql.query(String.format("SELECT * FROM `relationships` WHERE `player` = '%s' AND `state`='1';",p.getUniqueId()));
-        ComponentBuilder bc = new ComponentBuilder("");
-        int friends = 0;
-        while(results.next()){
-            friends++;
-            ProxyGamePlayer target;
-            UUID target_uuid=UUID.fromString(results.getString("target"));
-            target=ProxyGamePlayer.getProxyGamePlayer(target_uuid);
-            if(target.isonline()){
-                bc.append(Text.build("&a"+ ranks.valueOf(target.getRank().toString()).getColor()+target.getUsername()+"&r","Click to message","/msg "+target.getUsername()+" ", ClickEvent.Action.SUGGEST_COMMAND));
-                bc.append(Text.build(", "));
-            }else {
-                bc.append(Text.build("&7"+ target.getUsername()+"&r",Text.format("&cUser is offline")));
-                bc.append(Text.build(", "));
+        try (Jedis jedis = BungeeCore.pool.getResource()){
+            Set<String> friends = jedis.smembers(String.format("PLAYER|%s|relationships",p.getUniqueId()));
+            ComponentBuilder bc = new ComponentBuilder("");
+            int count = 0;
+            for(String s : friends){
+                count++;
+                ProxyGamePlayer target;
+                UUID target_uuid=UUID.fromString(s);
+                target=ProxyGamePlayer.getProxyGamePlayer(target_uuid);
+                if(target.isonline()){
+                    bc.append(Text.build("&a"+ ranks.valueOf(target.getRank().toString()).getColor()+target.getUsername()+"&r","Click to message","/msg "+target.getUsername()+" ", ClickEvent.Action.SUGGEST_COMMAND));
+                    bc.append(Text.build(", "));
+                }else {
+                    bc.append(Text.build("&7"+ target.getUsername()+"&r",Text.format("&cUser is offline")));
+                    bc.append(Text.build(", "));
+                }
             }
-        }
-
-        if(friends==0){
-            p.sendMessage(Text.build("&aFriends> &7You do not have any friends. Try &e/friends add&7!"));
-        }else {
-            String header = "&6-----------------------------------------------------\n\n&aFriend List\n";
-            p.sendMessage(Text.build(header));
-            p.sendMessage(Text.build(String.format("&aFriends (%s):",friends)));
-            p.sendMessage(bc.create());
-            p.sendMessage(Text.build("&6-----------------------------------------------------"));
+            if(count==0){
+                p.sendMessage(Text.build("&aFriends> &7You do not have any friends. Try &e/friends add&7!"));
+            }else {
+                String header = "&6-----------------------------------------------------\n\n&aFriend List\n";
+                p.sendMessage(Text.build(header));
+                p.sendMessage(Text.build(String.format("&aFriends (%s):",count)));
+                p.sendMessage(bc.create());
+                p.sendMessage(Text.build("&6-----------------------------------------------------"));
+            }
         }
     }
 }
